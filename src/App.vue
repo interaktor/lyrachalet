1cc222c9744ccff89e97d8c2b2bb497da4cb9b80
interaktor <xiaxhyu@gmail.com>
2026-09-15 17:09:01 +0800

    Updated the webpage structure

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,29 @@
   <div class="home">
     <!-- 新增 card 包裹所有内容！背景色加在这里 -->
     <div class="card">
-      <div class="name">
-        <span @click="changeName">{{ name }}</span>
-        <strong>{{ sign }}</strong>
-      </div>
       <ul class="list">
         <li><a href="#">Music</a></li>
         <li><a href="#">Book</a></li>
         <li><a href="#">Dev</a></li>
+        <li><a href="#">Picture</a></li>
+        <li><a href="#">Philosophy</a></li>
+        <li><a href="#">About</a></li>
       </ul>
+      <article></article>
+      <aside></aside>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+//Data
 let name = ref('Lyra ')
 let sign = ref('Think Different.')
+
+//Methods
 function changeName() {
   if (name.value === 'Lyra ') {
 
@@ -41,35 +47,48 @@ function changeName() {
   font-size: 20px;
 }
 
+a {
+  color: #999;
+  text-decoration: none;
+}
+
 .home {
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  height: 70vh;
-  /* 这里删掉背景！不要在这里加背景 */
+  /* flex-direction: column; */
+  height: 80vh;
 }
 
 /* 给card加背景，padding让内容不要贴边 */
 .card {
+  display: flex;
+  width: 600px;
+  height: 400px;
   background: #ffffff66;
-  padding: 30px 40px;
+  padding: 10px;
   border-radius: 12px;
   box-shadow: 0 0 5px #ccc;
-}
-
-.name {
-  margin-bottom: 10px;
 }
 
 .list {
   /* 删掉你之前的margin-left:-130px，这个会破坏居中！ */
   display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  width: 100px;
   gap: 20px;
+
 }
 
-a {
-  color: #999;
-  text-decoration: none;
+article {
+  display: flex;
+  flex: 1;
+}
+
+aside {
+  display: flex;
+  flex-shrink: 0;
+  width: 100px;
 }
 </style>
